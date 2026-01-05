a8fb91abcc6632fd4a160570fa73f14f8e2a0ff0
bizzbor <services@entropy-ai.in>
2026-01-05 12:09:43 +0530

    Fix merge conflict markers in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -176,17 +176,6 @@ export default function Navbar() {
                 height={1400}
                 priority
                 unoptimized
-<<<<<<< HEAD
-                className="object-contain h-16 md:h-20 lg:h-24 w-auto max-w-[190px] md:max-w-[180px] lg:max-w-[160px]"
-                style={{
-                  backgroundColor: 'transparent',
-                  display: 'block',
-                  height:'100px',
-                  width:auto,
-                  maxWidth:'200px',
-                  
-                  
-=======
                 className="object-contain"
                 style={{
                   backgroundColor: 'transparent',
@@ -194,7 +183,6 @@ export default function Navbar() {
                   width: 'auto',
                   maxWidth: '200px',
                   display: 'block'
->>>>>>> 3c36469 (Logo size increased to 100px)
                 }}
               />
             </div>
